887b803cc09623d120e0cdc8f0da4d639bc40640
Joel Griffith <joelgriffith@jgriffith.thetuscorp.com>
2013-02-19 10:44:56 -0800

    Added some legacy support. Still need IE7 fixes as well as window resize handlers.

--- sickholder.js
+++ sickholder.js
@@ -1,20 +1,29 @@
 // Load/Check app global space
 window.app = window.app || {};
 
-app.SickHolder = (function(){
+app.sickholder = (function(){
 
 	// Some 'global' functions & variables
-	var inputs, className = 'sick-holder';
+	var padding, inputs, className, fontSize, ie7;
 
 	// Application Object
 	var SickHolder = {
 
 		/*
 		 *	Instantiater, handles function calling and config
-		 *
 		 */
 		init: function(config){
-			config = config || {}; // TODO: Add config options
+			
+			// Load config options or use defaults
+			config = config || {};
+			padding = {
+				top : config.paddingTop || 0,
+				left : config.paddingLeft || 0
+			};
+			className = config.className || 'sick-holder';
+			fontSize = config.fontSize || '';
+
+			// Load inputs, generate placeholders
 			inputs = this.getElements();
 			this.createPlaceHolder(inputs);
 		},
@@ -31,27 +40,32 @@ app.SickHolder = (function(){
 		    	if (allElements[i].getAttribute('placeholder')){
 		      		matchingElements.push(allElements[i]);
 		    	}
-		  }
-		  return matchingElements;
+		  	}
+		  	return matchingElements;
 		},
 
 		/*
 		 * Creates labels that will act as placeholder shim
 		 * and attachs the event handlers
 		 *
+		 * TODO: Clean this up a bit, not DRY enough
+		 *
 		 * @param: Array of elements needing placeholder text
 		 */
 		createPlaceHolder: function(elems){
 			for(var i = 0; i < elems.length; i++){
 				var placeholderText = elems[i].getAttribute('placeholder');
-				var location = this.getElementPosition(elems[i]);
+				var placeholderID = elems[i].getAttribute('id');
+				var location = this.getPlaceholderPosition(elems[i]);
 				var label = document.createElement('label');
 				
 				// Generate the labels
 				label.innerHTML = placeholderText;
-				label.classList.add(className);
-				label.style.top = location.top + 'px';
-				label.style.left = location.left + 'px';
+				label.className = className;
+				label.setAttribute('for', placeholderID);
+				label.style.top = location.top + padding.top + 'px';
+				label.style.left = location.left + padding.left + 'px';
+				label.style.fontSize = fontSize;
 				this.insertAfter(elems[i], label);
 
 				// Attach the event handlers
@@ -66,36 +80,54 @@ app.SickHolder = (function(){
 		 * Handles the focus interaction
 		 *
 		 * @param: Element needing focus 
-		 *
 		 */
 		focusHandler: function(elem){
-			elem.addEventListener("focus", function(e) {
-				this.nextSibling.classList.add('focus');
-				return elem;
-		    });
+
+			// For modern browsers
+			if( document.addEventListener ){
+				elem.addEventListener("focus", function(e) {
+					this.nextSibling.className = className + ' focus';
+			    });
+
+			// Legacy browsers    
+			} else {
+				elem.attachEvent("focus", function(e) {
+					this.nextSibling.className = className + ' focus';
+			    });
+			}
+			return elem;
 		},
 
 		/*
 		 * Handles the blur interaction
 		 *
 		 * @param: Element needing blur handles 
-		 *
 		 */
 		blurHandler: function(elem){
-			elem.addEventListener("blur", function(e) {
-				this.nextSibling.classList.remove('focus');
-				return elem;
-		    });
+			
+			// For modern browsers
+			if( document.addEventListener ){
+				elem.addEventListener("blur", function(e) {
+					this.nextSibling.className = className;
+			    });
+
+			// Legacy browsers
+			} else {
+				elem.attachEvent("blur", function(e) {
+					this.nextSibling.className = className;
+			    });
+			}
+			return elem;
 		},
 
 		/*
 		 * Handles the keydown interaction
-		 *
-		 * @param: Array of elements needing keydown handles 
-		 *
+		 * 
+		 * @param: Array of elements needing keydown handles
 		 */
 		keyDownHandler: function(elem){
 			elem.onkeydown = function(e){
+				if( e == undefined ){ e = window.event } // For Legacy browsers
 				var key = e.keyCode;
 				if( !/[^A-Za-z0-9 ]/.test(String.fromCharCode(key)) ){
 					elem.nextSibling.style.display = "none";
@@ -106,12 +138,12 @@ app.SickHolder = (function(){
 
 		/*
 		 * Handles the keyup interaction
-		 *
-		 * @param: Array of elements needing keyup handles 
-		 *
+		 * 
+		 * @param: Array of elements needing keyup handles
 		 */
 		keyUpHandler: function(elem){
 			elem.onkeyup = function(e){
+				if( e == undefined ){ e = window.event } // For Legacy browsers
 				var key = e.keyCode;
 				if( elem.value == '' ){
 					elem.nextSibling.style.display = "block";
@@ -122,10 +154,10 @@ app.SickHolder = (function(){
 
 		/*
 		 * Method to get element position
-		 *
+		 * TODO: Currently broke in IE7/8
 		 * @return: Object with element coordination
 		 */
-		getElementPosition: function(elem){
+		getPlaceholderPosition: function(elem){
 			var position = {};
 			position.top = elem.offsetTop;
 			position.left = elem.offsetLeft;
